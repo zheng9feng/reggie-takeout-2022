41b372a75d3edfc6b6fc03b12902dea73af48c67
zheng9feng <zheng9fengm0v1@gmail.com>
2022-10-02 00:55:03 +0800

    servlet组件扫描注解

--- src/main/java/com/reggie/ReggieApplication.java
+++ src/main/java/com/reggie/ReggieApplication.java
@@ -3,12 +3,14 @@ package com.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author m0v1
  * @date 2022年09月30日 00:55
  */
 @Slf4j
+@ServletComponentScan // 使配置到filter被管理
 @SpringBootApplication
 public class ReggieApplication {
     public static void main(String[] args) {
