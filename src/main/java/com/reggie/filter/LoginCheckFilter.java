9973027b324ee3a3de991781d04699d991f9b4ac
zheng9feng <zheng9fengm0v1@gmail.com>
2022-10-09 19:54:28 +0800

    公共字段自动填充策略配置 5.通过过滤器将用户ID存储到BaseContext

--- src/main/java/com/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,7 @@
 package com.reggie.filter;
 
 import com.alibaba.fastjson.JSON;
+import com.reggie.common.BaseContext;
 import com.reggie.common.ResponseInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.AntPathMatcher;
@@ -57,8 +58,11 @@ public class LoginCheckFilter implements Filter {
         }
 
         // 4.判断登录状态，如果已登录，则直接放行
-        if (request.getSession().getAttribute("employee") != null) {
-            log.info("用户已登录，用户id为：{}", request.getSession().getAttribute("employee"));
+        Long employeeID = ((Long) request.getSession().getAttribute("employee"));
+        if (employeeID != null) {
+            log.info("用户已登录，用户id为：{}", employeeID);
+            BaseContext.setCurrentId(employeeID);
+
             filterChain.doFilter(request, response);
             return;
         }
@@ -66,7 +70,6 @@ public class LoginCheckFilter implements Filter {
         //5.如果未登录则返回未登录结果,通过输出流方式向客户端页面响应数据
         log.info("用户未登录");
         response.getWriter().write(JSON.toJSONString(ResponseInfo.error("NOTLOGIN")));
-        return;
     }
 
     /**
