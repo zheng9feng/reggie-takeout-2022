ee7a34849c3051e0d0f482a70348377b37c0de9c
zheng9feng <zheng9fengm0v1@gmail.com>
2022-10-16 16:48:22 +0800

    1.开启事务 2.修正注释

--- src/main/java/com/reggie/ReggieApplication.java
+++ src/main/java/com/reggie/ReggieApplication.java
@@ -4,13 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author m0v1
  * @date 2022年09月30日 00:55
  */
 @Slf4j
-@ServletComponentScan // 使配置到filter被管理
+@EnableTransactionManagement
+@ServletComponentScan // 使配置的filter被管理
 @SpringBootApplication
 public class ReggieApplication {
     public static void main(String[] args) {
