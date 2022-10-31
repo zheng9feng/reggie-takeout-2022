5a4d87681ab5a17fa72d16a3eede6ff2b047c45f
zheng9feng <zheng9fengm0v1@gmail.com>
2022-10-31 22:03:31 +0800

    docs: 数据库初始化脚本添加创建数据库命令

--- sql/db_reggie.sql
+++ sql/db_reggie.sql
@@ -12,26 +12,37 @@ File Encoding         : 65001
  
 Date: 2021-07-23 10:41:41
 */
- 
-SET FOREIGN_KEY_CHECKS=0;
+CREATE
+DATABASE
+IF NOT EXISTS `reggie`
+DEFAULT CHARACTER SET utf8mb4;
+
+USE
+`reggie`;
+
+SET NAMES utf8mb4;
+
+SET
+FOREIGN_KEY_CHECKS=0;
  
 -- ----------------------------
 -- Table structure for address_book
 -- ----------------------------
 DROP TABLE IF EXISTS `address_book`;
-CREATE TABLE `address_book` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
-  `user_id` bigint(20) NOT NULL COMMENT '用户id',
-  `consignee` varchar(50) COLLATE utf8_bin NOT NULL COMMENT '收货人',
-  `sex` tinyint(4) NOT NULL COMMENT '性别 0 女 1 男',
-  `phone` varchar(11) COLLATE utf8_bin NOT NULL COMMENT '手机号',
-  `province_code` varchar(12) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '省级区划编号',
-  `province_name` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '省级名称',
-  `city_code` varchar(12) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '市级区划编号',
-  `city_name` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '市级名称',
-  `district_code` varchar(12) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '区级区划编号',
-  `district_name` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '区级名称',
-  `detail` varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '详细地址',
+CREATE TABLE `address_book`
+(
+    `id`            bigint(20) NOT NULL COMMENT '主键',
+    `user_id`       bigint(20) NOT NULL COMMENT '用户id',
+    `consignee`     varchar(50) COLLATE utf8_bin NOT NULL COMMENT '收货人',
+    `sex`           tinyint(4) NOT NULL COMMENT '性别 0 女 1 男',
+    `phone`         varchar(11) COLLATE utf8_bin NOT NULL COMMENT '手机号',
+    `province_code` varchar(12) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '省级区划编号',
+    `province_name` varchar(32) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '省级名称',
+    `city_code`     varchar(12) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '市级区划编号',
+    `city_name`     varchar(32) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '市级名称',
+    `district_code` varchar(12) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '区级区划编号',
+    `district_name` varchar(32) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '区级名称',
+    `detail`        varchar(200) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '详细地址',
   `label` varchar(100) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '标签',
   `is_default` tinyint(1) NOT NULL DEFAULT '0' COMMENT '默认 0 否 1是',
   `create_time` datetime NOT NULL COMMENT '创建时间',
