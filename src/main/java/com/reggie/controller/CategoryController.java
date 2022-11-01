9c3708922465ae86041332cad946371d1f289885
zheng9feng <zheng9fengm0v1@gmail.com>
2022-11-01 11:58:44 +0800

    docs: 添加swagger相关注解

--- src/main/java/com/reggie/controller/CategoryController.java
+++ src/main/java/com/reggie/controller/CategoryController.java
@@ -4,6 +4,10 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.ResponseInfo;
 import com.reggie.entity.Category;
 import com.reggie.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +23,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/category")
+@Api(tags = "分类管理")
 public class CategoryController {
 
     @Autowired
@@ -32,6 +37,12 @@ public class CategoryController {
      * @return
      */
     @GetMapping("/page")
+    @ApiOperation("分页查询分类信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "当前页", required = true, paramType = "query", dataType = "int"),
+            @ApiImplicitParam(name = "pageSize", value = "每页行数", required = true, paramType = "query", dataType = "int")
+    })
+
     public ResponseInfo<Page<Category>> listCategory(int page, int pageSize) {
         log.info("分类信息分页查询:page = {}, pageSize = {}.", page, pageSize);
         return categoryService.listByPage(page, pageSize);
@@ -44,6 +55,7 @@ public class CategoryController {
      * @return
      */
     @PostMapping
+    @ApiOperation("新增分类")
     public ResponseInfo<String> addCategory(@RequestBody Category category) {
         log.info("新增分类:{}", category);
         categoryService.save(category);
@@ -57,6 +69,7 @@ public class CategoryController {
      * @return
      */
     @DeleteMapping
+    @ApiOperation("删除分类")
     public ResponseInfo<String> delete(Long categoryID) {
         log.info("将要删除的分类id:{}", categoryID);
 
@@ -71,6 +84,7 @@ public class CategoryController {
      * @return
      */
     @GetMapping("/list")
+    @ApiOperation("获取菜品分类列表")
     public ResponseInfo<List<Category>> listCategory(Category category) {
         return categoryService.listCategory(category);
     }
--- src/main/java/com/reggie/entity/Category.java
+++ src/main/java/com/reggie/entity/Category.java
@@ -2,6 +2,8 @@ package com.reggie.entity;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -14,48 +16,57 @@ import java.time.LocalDateTime;
  * @date 2022年10月10日 00:53
  */
 @Data
+@ApiModel("分类")
 public class Category implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @ApiModelProperty("主键")
     private Long id;
 
     /**
      * 类型 1-菜品分类 2-套餐分类
      */
+    @ApiModelProperty("类型")
     private Integer type;
 
     /**
      * 分类名称
      */
+    @ApiModelProperty("分类名称")
     private String name;
 
     /**
      * 顺序
      */
+    @ApiModelProperty("顺序")
     private Integer sort;
 
     /**
      * 创建时间
      */
     @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty("创建时间")
     private LocalDateTime createTime;
 
     /**
      * 更新时间
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty("更新时间")
     private LocalDateTime updateTime;
 
     /**
      * 创建人
      */
     @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty("创建人")
     private Long createUser;
 
     /**
      * 修改人
      */
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @ApiModelProperty("修改人")
     private Long updateUser;
 }
